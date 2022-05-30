cfa1ed0640e262eba8aa17f65b319a45492ca068
Seb-figment <sebastien.nobert@figment.io>
2022-05-30 18:24:30 -0400

    no-namespace

--- index.ts
+++ index.ts
@@ -1,14 +1,11 @@
 import { Protobuf } from "as-proto";
-import { foo } from "./src/foo/baz/foo"
-import { bar } from "./src/bar/bar";
-import { bar as bar_2 } from "./src/bar/qux";
-import { foo as foo_2 } from "./src/foo/baz/qux"
+import { foo, bar } from "./src/"
 
 const a: bar.Bar = new bar.Bar(
     new Uint8Array(4),
-    [ new bar_2.Qux(3, "bar_2.Qux", null)],
-    [ new foo_2.baz.Qux(
+    [ new bar.Qux(3, "bar.Qux", null)],
+    [ new foo.baz.Qux(
         new foo.baz.Foo(1, 2), "foo.baz.Foo"
     )]);
 const encoded = Protobuf.encode<bar.Bar>(a, bar.Bar.encode)
-const b: bar.Bar = Protobuf.decode<bar.Bar>(encoded, bar.Bar.decode);
+const b: bar.Bar = bar.decodeBar(encoded);
